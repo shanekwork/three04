183041262695d2c1342bd8e4bf70a3d937a7282a
Shane Kelly <shanekweb@icloud.com>
2016-02-17 14:22:28 +0000

    testing elastic

--- config/initializers/bonsai.rb
+++ config/initializers/bonsai.rb
@@ -1,1 +1,1 @@
-Elasticsearch::Model.client = Elasticsearch::Client.new url: ENV['https://nb8xr6bf:gx82mukpp2rgwajl@apricot-5267517.us-east-1.bonsai.io'], log: true
+Elasticsearch::Model.client = Elasticsearch::Client.new url: ENV['BONSAI_URL'], log: true
